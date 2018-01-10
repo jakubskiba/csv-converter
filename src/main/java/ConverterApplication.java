cf41c3eb8ce4da08c46b332ffe69f2bd2f09cf79
jakubskiba <jakubskibakrk@gmail.com>
2018-01-10 23:05:15 +0100

    handle 0 argument situation

--- src/main/java/ConverterApplication.java
+++ src/main/java/ConverterApplication.java
@@ -1,5 +1,9 @@
 public class ConverterApplication {
     public static void main(String[] args) {
-        
+        switch (args.length) {
+            case 0:
+                System.err.println("No input file defined");
+                break;
+        }
     }
 }
