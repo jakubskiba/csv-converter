30e4868bb959939875c818574404f7b84fa589f1
jakubskiba <jakubskibakrk@gmail.com>
2018-01-11 04:16:15 +0100

    implement CsvRow

--- src/main/java/com/codecool/model/CsvRow.java
+++ src/main/java/com/codecool/model/CsvRow.java
@@ -1,7 +1,20 @@
 package com.codecool.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CsvRow {
     private List<String> elementList;
+
+    public CsvRow(String[] elements) {
+        this(Arrays.asList(elements));
+    }
+
+    public CsvRow(List<String> elementList) {
+        this.elementList = elementList;
+    }
+
+    public List<String> getElementList() {
+        return elementList;
+    }
 }
