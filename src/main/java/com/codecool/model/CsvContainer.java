136c9b6aa434a75ca599a18725be56d047981609
jakubskiba <jakubskibakrk@gmail.com>
2018-01-11 04:16:54 +0100

    implement csv containter

--- src/main/java/com/codecool/model/CsvContainer.java
+++ src/main/java/com/codecool/model/CsvContainer.java
@@ -1,8 +1,30 @@
 package com.codecool.model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class CsvContainer {
     private List<CsvRow> rows;
     private List<String> headers;
+
+    public CsvContainer() {
+        this.rows = new LinkedList<>();
+        this.headers = new LinkedList<>();
+    }
+
+    public List<CsvRow> getRows() {
+        return rows;
+    }
+
+    public void createRow(List<String> rowElements) {
+        this.rows.add(new CsvRow(rowElements));
+    }
+
+    public List<String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(List<String> headers) {
+        this.headers = headers;
+    }
 }
