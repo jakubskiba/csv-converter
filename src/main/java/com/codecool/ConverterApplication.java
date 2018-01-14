81ebbfc5bae01bfcde1e611d7ac9cab95c355f37
jakubskiba <jakubskibakrk@gmail.com>
2018-01-14 06:16:25 +0100

    implement springcontext into project

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -1,46 +1,29 @@
 package com.codecool;
 
-import com.codecool.model.OutputFormat;
+import com.codecool.exceptions.InvalidArgumentsSyntaxException;
 import com.codecool.model.ParsedArguments;
-import com.codecool.service.FileReader;
-import com.codecool.service.OutputFormatterFactory;
+import com.codecool.service.ArgumentParser;
 import com.codecool.service.SimpleCsvConverter;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
-import java.util.List;
 
 public class ConverterApplication {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.err.println("No input file defined!");
-        } else {
-            ParsedArguments parsedArguments = parseArgs(Arrays.asList(args));
-            SimpleCsvConverter simpleCsvConverter = new SimpleCsvConverter(new FileReader(), new OutputFormatterFactory());
-            try {
-                simpleCsvConverter.convert(parsedArguments);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("sccContext.xml");
+
+            ArgumentParser argumentParser = (ArgumentParser) ctx.getBean("argumentParser");
+            ParsedArguments parsedArguments = argumentParser.parseArgs(Arrays.asList(args));
+
+            SimpleCsvConverter simpleCsvConverter = (SimpleCsvConverter) ctx.getBean("simpleCsvConverter");
+
+            simpleCsvConverter.convert(parsedArguments);
+
+        } catch (FileNotFoundException | InvalidArgumentsSyntaxException e) {
+            System.err.println(e.getMessage());
         }
 
-    }
-
-    private static ParsedArguments parseArgs(List<String> args) {
-        if (args.size() == 1) {
-            return new ParsedArguments(OutputFormat.TABLE, getFile(args.get(0)), new ArrayList<>());
-        } else {
-            return new ParsedArguments(getOutputFormat(args.get(0)), getFile(args.get(1)), args.subList(1, args.size()));
-        }
-    }
-
-    private static OutputFormat getOutputFormat(String outputFormat) {
-        return OutputFormat.valueOf(outputFormat.toUpperCase());
-    }
-
-    private static File getFile(String filePath) {
-        return new File(filePath);
     }
 }
